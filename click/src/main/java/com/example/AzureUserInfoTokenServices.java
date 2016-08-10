d9feb2d73cd025d89cb51134471c487a6b337b6e
crazyquark <cristian.sandu@gmail.com>
2016-08-10 11:20:29 +0300

    downgrade spring

--- click/src/main/java/com/example/AzureUserInfoTokenServices.java
+++ click/src/main/java/com/example/AzureUserInfoTokenServices.java
@@ -1,27 +1,17 @@
 package com.example;
 
+import java.util.Map;
+
 import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.oauth2.common.exceptions.InvalidTokenException;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
 
 public class AzureUserInfoTokenServices extends UserInfoTokenServices {
 
 	public AzureUserInfoTokenServices(String userInfoEndpointUrl, String clientId) {
 		super(userInfoEndpointUrl, clientId);
-		
-		this.setPrincipalExtractor(new AzurePrincipalExtractor());
 	}
-	
+
 	@Override
-	public OAuth2Authentication loadAuthentication(String accessToken)
-			throws AuthenticationException, InvalidTokenException {
-		OAuth2Authentication token = super.loadAuthentication(accessToken);
-		
-//		UsernamePasswordAuthenticationToken authToken =
-//				(UsernamePasswordAuthenticationToken) token.getUserAuthentication();
-//		
-		
-		return token;
+	protected Object getPrincipal(Map<String, Object> map) {
+		return super.getPrincipal(map);
 	}
 }
